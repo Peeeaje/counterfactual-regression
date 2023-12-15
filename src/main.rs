8af7c31c524b6ac7ba72a72a2f71d788151e9fa5
Peeeaje <74146834+Peeeaje@users.noreply.github.com>
2023-12-16 00:50:47 +0900

    最後まで仮実装完了(正しく動いてない）

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,98 @@
 use std::collections::HashMap;
 
 const N_ACTIONS: i32 = 2;
+const N_ITERATIONS: i32 = 10000;
 const N_CARDS: i32 = 3;
 
+#[derive(Clone)]
 struct InformationSet {
-    strategy: Vec<f32>,
+    key: String,
     regret_sum: Vec<f32>,
+    strategy_sum: Vec<f32>,
+    strategy: Vec<f32>,
     reach_pr: f32,
+    reach_pr_sum: f32,
 }
 
 impl InformationSet {
-    fn next_strategy(&self) -> Vec<f32> {
-        return self.strategy;
+    fn next_strategy(&mut self) {
+        self.strategy_sum = self
+            .strategy_sum
+            .iter()
+            .zip(self.strategy.iter())
+            .map(|(&s_sum, &s)| s_sum + self.reach_pr * s)
+            .collect::<Vec<f32>>();
+        self.strategy = self.calc_strategy();
+        self.reach_pr_sum += self.reach_pr;
+        self.reach_pr = 0.0;
+    }
+    fn calc_strategy(&self) -> Vec<f32> {
+        let mut strategy = self.make_positive(&self.regret_sum);
+        let total: f32 = strategy.iter().sum();
+        if total > 0.0 {
+            for s in strategy.iter_mut() {
+                *s /= total;
+            }
+        } else {
+            for s in strategy.iter_mut() {
+                *s = 1.0 / N_ACTIONS as f32;
+            }
+        }
+        strategy
+    }
+    fn get_average_strategy(&self) -> Vec<f32> {
+        let total_strategy = self
+            .strategy_sum
+            .iter()
+            .map(|s| s / self.reach_pr_sum)
+            .map(|s| if s > 0.001 { s } else { 0.0 })
+            .collect::<Vec<f32>>();
+
+        let total: f32 = total_strategy.iter().sum();
+        let mut average_strategy = total_strategy;
+        average_strategy.iter_mut().for_each(|s| *s /= total as f32);
+        average_strategy
+    }
+    fn make_positive(&self, regret_sum: &Vec<f32>) -> Vec<f32> {
+        let mut positive_regret_sum = regret_sum.clone();
+        positive_regret_sum
+            .iter_mut()
+            .for_each(|r| *r = if *r > 0.0 { *r } else { 0.0 });
+        positive_regret_sum
+    }
+}
+
+impl std::fmt::Display for InformationSet {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let strategies = self.get_average_strategy();
+        write!(
+            f,
+            "{} {} {}",
+            self.key,
+            strategies[0].to_string(),
+            strategies[1].to_string(),
+        )
     }
 }
 
 /// counterfactual regret minimizationのiterationを行う
 fn main() {
     let mut i_map: HashMap<String, InformationSet> = HashMap::new();
-    const N_ITERATIONS: i32 = 10000;
     let mut expected_game_value = 0.0;
 
-    for i in 1..N_ITERATIONS {
-        expected_game_value += cfr(i_map, String::from(""), -1, -1, 1.0, 1.0, 1.0);
-        for (key, value) in i_map.iter_mut() {
+    for _i in 1..N_ITERATIONS {
+        expected_game_value += cfr(&mut i_map, String::from(""), -1, -1, 1.0, 1.0, 1.0);
+        for (_key, value) in i_map.iter_mut() {
             value.next_strategy();
         }
     }
-
     expected_game_value /= N_ITERATIONS as f32;
-
-    println!("expected game value: {}", expected_game_value);
+    display_result(expected_game_value, &i_map);
 }
 
 /// counterfactual regret minimization algorithm
 fn cfr(
-    i_map: HashMap<String, InformationSet>,
+    mut i_map: &mut HashMap<String, InformationSet>,
     history: String,
     card_1: i32,
     card_2: i32,
@@ -43,12 +100,12 @@ fn cfr(
     pr_2: f32,
     pr_c: f32,
 ) -> f32 {
-    if is_chance_node(history) {
+    if is_chance_node(&history) {
         return chance_util(i_map);
     }
 
-    if is_terminal(history) {
-        return terminal_util(history, card_1, card_2);
+    if is_terminal(&history) {
+        return terminal_util(&history, card_1, card_2);
     }
 
     let n: usize = history.len();
@@ -56,9 +113,9 @@ fn cfr(
 
     let mut info_set: InformationSet;
     if is_player_1 {
-        let info_set = get_info_set(i_map, card_1, history);
+        info_set = get_info_set(&mut i_map, card_1, &history);
     } else {
-        let info_set = get_info_set(i_map, card_2, history);
+        info_set = get_info_set(&mut i_map, card_2, &history);
     }
 
     let strategy = info_set.strategy;
@@ -70,14 +127,14 @@ fn cfr(
     }
 
     // counterfactual utility per action
-    let action_utils = vec![0.0; N_ACTIONS as usize];
+    let mut action_utils = vec![0.0; N_ACTIONS as usize];
 
     for (i, action) in vec!["c", "b"].iter().enumerate() {
         let next_history = format!("{}{}", history, action);
 
         if is_player_1 {
             action_utils[i] = -cfr(
-                i_map,
+                &mut i_map,
                 next_history,
                 card_1,
                 card_2,
@@ -87,7 +144,7 @@ fn cfr(
             );
         } else {
             action_utils[i] = -cfr(
-                i_map,
+                &mut i_map,
                 next_history,
                 card_1,
                 card_2,
@@ -124,19 +181,19 @@ fn cfr(
     return util;
 }
 
-fn is_chance_node(history: String) -> bool {
+fn is_chance_node(history: &str) -> bool {
     return history == "";
 }
 
-fn chance_util(i_map: HashMap<String, InformationSet>) -> f32 {
+fn chance_util(i_map: &mut HashMap<String, InformationSet>) -> f32 {
     let mut expected_value = 0.0;
     let n_possibilities = 6;
-    for i in 1..N_CARDS {
+    for _i in 1..N_CARDS {
         for j in 1..N_CARDS {
             expected_value += cfr(
                 i_map,
                 "rr".to_string(),
-                i,
+                _i,
                 j,
                 1.0,
                 1.0,
@@ -147,7 +204,88 @@ fn chance_util(i_map: HashMap<String, InformationSet>) -> f32 {
     expected_value / n_possibilities as f32
 }
 
-fn is_terminal(history: String) -> bool {
+fn is_terminal(history: &str) -> bool {
     let possible_terminal = vec!["rrcc", "rrcbc", "rrcbb", "rrbc", "rrbb"];
-    possible_terminal.contains(&history.as_str())
+    possible_terminal.contains(&history)
+}
+
+fn terminal_util(history: &str, card_1: i32, card_2: i32) -> f32 {
+    let player_card = if history.len() / 2 == 0 {
+        card_1
+    } else {
+        card_2
+    };
+    let opponent_card = if history.len() / 2 == 0 {
+        card_2
+    } else {
+        card_1
+    };
+
+    if (history == "rrcbc") | (history == "rrbc") {
+        return 1.0;
+    } else if history == "rrcc" {
+        return if player_card > opponent_card {
+            1.0
+        } else {
+            -1.0
+        };
+    } else {
+        return if player_card > opponent_card {
+            2.0
+        } else {
+            -2.0
+        };
+    }
+}
+
+fn card_str(card: i32) -> String {
+    match card {
+        0 => return "J".to_string(),
+        1 => return "Q".to_string(),
+        2 => return "K".to_string(),
+        _ => return "".to_string(),
+    }
+}
+
+fn get_info_set(
+    i_map: &mut HashMap<String, InformationSet>,
+    card: i32,
+    history: &str,
+) -> InformationSet {
+    let key = format!("{} {}", card_str(card), history);
+    let info_set = i_map.get(&key);
+
+    if info_set.is_none() {
+        let new_info_set = InformationSet {
+            key: key.clone(),
+            regret_sum: vec![0.0; N_ACTIONS as usize],
+            strategy_sum: vec![0.0; N_ACTIONS as usize],
+            strategy: vec![0.0; N_ACTIONS as usize],
+            reach_pr: 0.0,
+            reach_pr_sum: 0.0,
+        };
+        i_map.insert(key.clone(), new_info_set);
+    }
+    return i_map.get(&key.clone()).unwrap().clone();
+}
+
+fn display_result(expected_game_value: f32, i_map: &HashMap<String, InformationSet>) {
+    println!("player 1 expected game value: {}", expected_game_value);
+    println!("player 2 expected game value: {}", -expected_game_value);
+
+    println!("player 1 strategy:");
+    let mut items: Vec<_> = i_map.iter().collect();
+    items.sort_by(|a, b| a.0.cmp(&b.0));
+    for (key, value) in items.iter() {
+        if key.len() % 2 == 0 {
+            println!("{}", value);
+        }
+    }
+
+    println!("player 2 strategy:");
+    for (key, value) in items.iter() {
+        if key.len() % 2 == 1 {
+            println!("{}", value);
+        }
+    }
 }
